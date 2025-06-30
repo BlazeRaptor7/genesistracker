5ae9e673226de61deac14148f1eb89f605c52c80
BlazeRaptor7 <arijeet@lampros.tech>
2025-06-30 14:51:51 +0530

    Update cards2.py

--- streamlit/cards2.py
+++ streamlit/cards2.py
@@ -33,7 +33,7 @@ st.markdown("""
         .stApp{
             width: 100vw;
             box-sizing: border-box;
-            background: linear-gradient(120deg, #f2f4f6, #e0e7ec);
+            background: linear-gradient(120deg, #5497c8, #285f87);
             background-size: cover;
             background-attachment: fixed;
             background-position: center;
